4f6a03b00d2fff35d341fbc24bc90e0b31e18acc
Chudleyj <chudleyjustin@gmail.com>
2018-04-04 14:53:41 -0400

    Added early stage buy and sell functionality. Cleaned up code.

--- BinanceBot.cpp
+++ BinanceBot.cpp
@@ -5,113 +5,127 @@
 #include <iostream>
 #include <string>
 #include <chrono>
+#include <sstream>
+#include <stdio.h>
 
 using namespace utility;                    // Common utilities like string conversions
 using namespace web;                        // Common features like URIs.
 using namespace web::http;                  // Common HTTP functionality
 using namespace web::http::client;          // HTTP client features
 using namespace concurrency::streams;       // Asynchronous streams
-using namespace web;
-using namespace web::websockets::client;
 using namespace std;
-using namespace std::chrono;
+using namespace std::chrono;                //Allows us to get epoch time without having to ping Binance server for it
 
-void print_test(json::value const &);
-void search();
-void test();
-string getTime();
-string HMACsha256(string);
+string getTime(); //Get epoch time (It will be needed for the signature)
+string HMACsha256(string const&, string const&); //Generate HMACsha256 signature
+void init();
+void tradingBot();
+void getPrice(const string);
+void formatPrice(json::value const &);
 
 int main(int argc, char* argv[])
 {
-    search();
+    cout << endl << "Welcome to the C++ Binance Bot. WARNING --> This is still an extremly early alpha build. Use at your own risk." << endl;
+    init();
+   // search();
     return 0;
 }
 
-void search(){
+void init(){
+    string input;
+    cout << endl << "Choose bot mode: 1 = trading" << endl;
+    cin >> input;
+    (input == "1") ? tradingBot() : init();
+}
+
+void tradingBot(){
+    string pair, choice, price, amount;
     
-    string coin,target = "/api/v3/ticker/price?symbol=";
-   // cout << "Enter coin pairing (EX: LTCBTC)";
-   // cin >> coin;
-    target = target+coin;
-   // cout << target;
+    cout << "Select trading pair (e.g. LTCBTC, ETHBTC,TRXETH, etc.)" << endl;
+    cin >> pair;
+    
+    cout << "Selected " << pair << ". Last known price of " << pair << ": ";
+    getPrice(pair);
+    
+    cout << "Buying or selling? (1 for buying, 0 for selling)" << endl;
+    cin >> choice;
+    (choice == "0") ? choice = "SELL" : choice = "BUY";
+    
+    cout << "Enter the price you would like to buy/sell at." << endl;
+    cin >> price;
+    
+    cout << "Enter amount you would like to buy/sell." << endl;
+    cin >> amount;
+    
     string curlTest = "curl -H \"X-MBX-APIKEY: ";
-    string secret = "YOURPRIVATEKEYHERE\" "; //THIS IS THE KEY FOR THE HMAC SHA256
-    /* THE MESSAGE for it is:
-        'https://api.binance.com/api/v3/order/test?symbol=LTCBTC&side=BUY&type=LIMIT&timeInForce=GTC' -d 'quantity=1&price=0.1&recvWindow=6000000&timestamp=WHATEVERTHECURRENTEPOCHTIMEIS;&signature=69452f1d9ac1ff6cfada72cdd81b025372b3841e52a747a00eb4dfcde7203853'"
-     */
-    string command = "-X POST 'https://api.binance.com/api/v3/order/test?symbol=LTCBTC&side=BUY&type=LIMIT&timeInForce=GTC' -d 'quantity=1&price=0.1&recvWindow=6000000&timestamp=";
+    string secret = "YOURSECRETKEY"; //THIS IS THE KEY FOR THE HMAC SHA256
+    string APIKEY = "YOURPUBLICKEY";
+    string command = "\" -X POST 'https://api.binance.com/api/v3/order?symbol="+pair+"&side="+choice+"&type=LIMIT&timeInForce=GTC&quantity="+amount+"&price="+price+"&recvWindow=5000&timestamp=";
     string time = getTime();
-    string sig = "&signature=";
-    string message = "symbol=LTCBTC&side=BUY&type=LIMIT&timeInForce=GTC' -d 'quantity=1&price=0.1&recvWindow=6000000&timestamp=" + time;
-    string api = HMACsha256(message); //THIS IS THE HMAC SHA256 SIGNATURE. IT NEEDS TO CHANGE EVERY TIME "time" CHANGES
-    curlTest = curlTest+secret+command+time+sig+api;
-   // cout << endl << endl << curlTest << endl << endl;
+    string message = "symbol="+pair+"&side="+choice+"&type=LIMIT&timeInForce=GTC&quantity="+amount+"&price="+price+"&recvWindow=5000&timestamp="+time;
+    auto api = HMACsha256(message, secret); //THIS IS THE HMAC SHA256 SIGNATURE. USES MESSAGE AND YOUR SECRET API KEY
+    curlTest = curlTest+APIKEY+command+time+"&signature="+api+"'";
+    cout << endl << endl << curlTest << endl << endl;
     system(curlTest.c_str());
+    
+    
 }
-string HMACsha256(string message){
-    // The secret key for hashing
-    const char key[] = "YOURPRIVATEKEYHERE";
+
+void printPrice(json::value const & value){
+    if(!value.is_null()){
+        json::value test = value;
+        cout << test["price"] << endl;
+    }
     
-    // The data that we're going to hash
-    char data[] = message;
-    
-    // Be careful of the length of string with the choosen hash engine. SHA1 needed 20 characters.
-    // Change the length accordingly with your choosen hash engine.
-    unsigned char* result;
-    unsigned int len = 64;
-    
-    result = (unsigned char*)malloc(sizeof(char) * len);
-    
-    HMAC_CTX ctx;
-    HMAC_CTX_init(&ctx);
-    
-    HMAC_Init_ex(&ctx, key, strlen(key), EVP_sha256(), NULL);
-    HMAC_Update(&ctx, (unsigned char*)&data, strlen(data));
-    HMAC_Final(&ctx, result, &len);
-    HMAC_CTX_cleanup(&ctx);
-    
-    printf("HMAC digest: ");
-    
-    for (int i = 0; i != len; i++)
-        printf("%02x", (unsigned int)result[i]);
-    
-    printf("\n");
-    
-    free(result);
 }
+
+void getPrice(const string pair){
+    http_client client(U("https://api.binance.com/"));
+    string full_request = "/api/v3/ticker/price?symbol=" + pair;
+    client.request(methods::GET, full_request).then([](http_response response)->
+                                                      pplx::task<json::value>{
+                                                          if(response.status_code() == status_codes::OK){
+                                                              return response.extract_json();
+                                                          }
+                                                          return pplx::task_from_result(json::value());
+                                                      })
+    .then([](pplx::task<json::value> previousTask){
+        try{
+            json::value const & v = previousTask.get();
+            printPrice(v);
+        }
+        catch (http_exception const & e){
+            wcout << e.what() << endl;
+        }
+    }).wait();
+}
+
+inline char binary_to_hex_digit(unsigned a) {
+    return a + (a < 10 ? '0' : 'a' - 10);
+}
+
+std::string binary_to_hex(unsigned char const* binary, unsigned binary_len) {
+    string r(binary_len * 2, '\0');
+    for(unsigned i = 0; i < binary_len; ++i) {
+        r[i * 2] = binary_to_hex_digit(binary[i] >> 4);
+        r[i * 2 + 1] = binary_to_hex_digit(binary[i] & 15);
+    }
+    return r;
+}
+
+string HMACsha256(string const& message, string const& key) {
+    unsigned char result[EVP_MAX_MD_SIZE];
+    unsigned result_len = 0;
+    HMAC(EVP_sha256(), key.data(), key.size(), reinterpret_cast<unsigned char const*>(message.data()), message.size(), result, &result_len);
+    return binary_to_hex(result, result_len);
+}
+
+
+
 string getTime(){
     struct timeval tp;
     gettimeofday(&tp, NULL);
     long long mslong = (long long) tp.tv_sec * 1000L + tp.tv_usec / 1000;
     auto time = to_string(mslong);
     return time;
-}
-
-void test(){
-    http_client client(U("https://api.binance.com/"));
-    client.request(methods::GET, "/api/v1/time").then([](http_response response)->
-                                              pplx::task<json::value>{
-                                                  if(response.status_code() == status_codes::OK){
-                                                      return response.extract_json();
-                                                  }
-                                                  return pplx::task_from_result(json::value());
-                                              })
-    .then([](pplx::task<json::value> previousTask){
-        try{
-            json::value const & v = previousTask.get();
-            print_test(v);
-        }
-        catch (http_exception const & e){
-            wcout << e.what() << endl;
-        }
-    }).wait();
-    
-}
-void print_test(json::value const & value){
-    if(!value.is_null()){
-        json::value test = value;
-       cout << test["serverTime"] << endl << time;
-    }
-    
 }
